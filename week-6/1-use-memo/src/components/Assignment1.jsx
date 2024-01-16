08b7b8104a575c31a5c7825ec41ddb81c49e884f
Rishit Saharan <rishitsaharanbiz@gmail.com>
2024-01-16 08:57:40 +0530

    Week6 assignments done

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
@@ -6,7 +6,19 @@ import { useState } from "react";
 export function Assignment1() {
     const [input, setInput] = useState(0);
     // Your solution starts here
-    const expensiveValue = 0; 
+    let expensiveValue = 0; 
+    useMemo(() => {
+        if(input == 0){
+            expensiveValue = 0;
+        }
+        else{
+            let factorial = 1;
+            for(let i = 2; i<=input; i++){
+                factorial *= i;
+            }
+            expensiveValue = factorial;
+        }
+    }, [input]);
     // Your solution ends here
 
     return (
--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from "react";
+import { useRef, useEffect } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
-
+    const myRef = useRef();
     useEffect(() => {
-
+        myRef.current.focus();
     }, []);
 
     const handleButtonClick = () => {
-
+        myRef.current.focus();
     };
 
     return (
         <div>
-            <input type="text" placeholder="Enter text here" />
+            <input ref= {myRef} type="text" placeholder="Enter text here" />
             <button onClick={handleButtonClick}>Focus Input</button>
         </div>
     );
